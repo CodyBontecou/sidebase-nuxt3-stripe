fb5d34ba1dbf2ad36fe748cbeb8a011160f114c6
Cody Bontecou <bontecouc@gmail.com>
2023-03-11 21:45:02 -1000

    dependencies + env variables

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -21,7 +21,10 @@ export default defineNuxtConfig({
   runtimeConfig: {
     STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY,
     STRIPE_SIGNING_SECRET: process.env.STRIPE_SIGNING_SECRET,
+    GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET,
+    API_ROUTE_SECRET: process.env.API_ROUTE_SECRET,
     public: {
+      GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
       STRIPE_PUBLISHABLE_KEY: process.env.STRIPE_PUBLISHABLE_KEY
     }
   }

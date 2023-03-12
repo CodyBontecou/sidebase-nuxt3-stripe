d6cfad2f92d58f57d114aa51a436a6df71959b0d
Cody Bontecou <bontecouc@gmail.com>
2023-03-11 21:45:12 -1000

    Initial UI Components

---
+++ components/PricingCard.vue
@@ -0,0 +1,103 @@
+<script setup lang="ts">
+import { loadStripe } from '@stripe/stripe-js'
+
+const { data, signIn } = useSession()
+
+const runtimeConfig = useRuntimeConfig()
+const { plan } = defineProps(['plan'])
+console.log(toRaw(plan))
+
+async function login () {
+  await signIn('github')
+}
+
+const showSubscribeButton = computed(
+  () => !!data.value?.user && !data.value.is_subscribed
+)
+const showCreateAccountButton = computed(() => !data.value?.user)
+const showManageSubscriptionButton = computed(
+  () => !!data.value?.user && data.value?.is_subscribed
+)
+
+async function processSubscription (priceId: number) {
+  const { data: stripeSessionData } = await useFetch(
+    `api/subscription/${priceId}`
+  )
+
+  const stripe = await loadStripe(runtimeConfig.public.STRIPE_PUBLISHABLE_KEY)
+  await stripe.redirectToCheckout({
+    sessionId: stripeSessionData.value.stripeSessions.id
+  })
+}
+</script>
+
+<template>
+  <div
+    class="mx-auto flex max-w-lg flex-col rounded-lg border border-gray-600 bg-gray-800 p-6 text-center text-white shadow xl:p-8"
+  >
+    <h3 class="mb-4 text-2xl font-semibold">
+      {{ plan.name }}
+    </h3>
+    <p class="font-light text-gray-400 sm:text-lg">
+      {{ plan.description }}
+    </p>
+    <div class="my-8 flex items-baseline justify-center">
+      <span class="mr-2 text-5xl font-extrabold">${{ plan.price / 100 }}</span>
+      <span class="text-gray-400">/{{ plan.interval }}</span>
+    </div>
+    <!-- List -->
+    <ul role="list" class="mb-8 space-y-4 text-left">
+      <li
+        class="flex items-center space-x-3"
+      >
+        <!-- Icon -->
+        <Checkmark />
+        <span>Feature text here</span>
+      </li>
+      <li
+        class="flex items-center space-x-3"
+      >
+        <!-- Icon -->
+        <Checkmark />
+        <span>And another feature</span>
+      </li>
+      <li
+        class="flex items-center space-x-3"
+      >
+        <!-- Icon -->
+        <Checkmark />
+        <span>Featuuuures</span>
+      </li>
+      <li
+        class="flex items-center space-x-3"
+      >
+        <!-- Icon -->
+        <Checkmark />
+        <span>$$$</span>
+      </li>
+    </ul>
+    <button
+      v-if="showSubscribeButton"
+      class="btn"
+      @click="processSubscription(plan.id)"
+    >
+      Subscribe
+    </button>
+    <button v-else-if="showCreateAccountButton" class="btn" @click="login">
+      Create Account
+    </button>
+    <NuxtLink
+      v-else-if="showManageSubscriptionButton"
+      to="/dashboard"
+      class="btn"
+    >
+      Manage Subscription
+    </NuxtLink>
+  </div>
+</template>
+
+<style scoped>
+.btn {
+  @apply rounded-lg bg-indigo-500 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-indigo-600 focus:ring-4 focus:ring-indigo-900;
+}
+</style>
---
+++ components/PricingSection.vue
@@ -0,0 +1,24 @@
+<script setup>
+const { data: plans } = await useFetch('/api/stripe/plans')
+</script>
+
+<template>
+  <section class="">
+    <div class="mx-auto max-w-screen-xl py-8 px-4 lg:py-16 lg:px-6">
+      <div class="mx-auto mb-8 max-w-screen-md text-center lg:mb-12">
+        <h2 class="mb-4 text-4xl font-extrabold tracking-tight text-white">
+          Designed for gamers
+        </h2>
+        <p class="mb-5 font-light text-gray-400 sm:text-xl">
+          Here at Costream we focus on technology and innovation that can unlock
+          streamings real potential.
+        </p>
+      </div>
+      <div
+        class="space-y-8 sm:gap-6 lg:grid lg:grid-cols-3 lg:space-y-0 xl:gap-10"
+      >
+        <PricingCard v-for="plan in plans" :key="plan.id" :plan="plan" />
+      </div>
+    </div>
+  </section>
+</template>
